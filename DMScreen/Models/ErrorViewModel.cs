10cece5cd6b0e921e052b35a6ea6055b77632327
Pojean <mijn@outlook.dk>
2026-08-02 18:43:14 +0200

    Add project files.

---
+++ DMScreen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DMScreen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
