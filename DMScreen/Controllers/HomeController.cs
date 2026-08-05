4aa7aad02fe1b7de784b387990d56e3458037e67
Pojean <mijn@outlook.dk>
2026-08-05 13:44:15 +0200

    View with Effect Creation form

--- DMScreen/Controllers/HomeController.cs
+++ DMScreen/Controllers/HomeController.cs
@@ -15,28 +15,18 @@ namespace DMScreen.Controllers
 
         public IActionResult Index()
         {
-            EffectLibrary elib = new EffectLibrary();
-            elib.effectsLibrary.Add(new Effect { Type = "Utility", Tier = "Uncommon", Description = "As an action, you can conjure a 20-foot radius sphere of errors, all creatures inside the sphere get very frustrated." });
-            elib.SaveLibrary();
-
-            EffectLibrary elib2 = new EffectLibrary();
-            elib2.LoadLibrary();
-
-            ItemLibrary ilib = new ItemLibrary();
-            ilib.itemLibrary.Add(new Item
-            {
-                Name = "The Ingenburger Legacy",
-                Rarity = "Very Rare",
-                EffectSlots = 4,
-                Description = "The Ingenburger Legacy is the pinnacle of spatulas. Able to flip any burger patty, no matter how stuck to the grill it is.",
-                Effects = { new Effect {Name = "The Flip", Tier ="Very Rare", Type = "Offense", Description = "As part of an attack, slide the spatula under an enemy, flipping them prone."}, new Effect {Name = "The Slide", Tier = "Rare", Type = "Defense",
-                Description = "When hit by an attack, as a reaction, use the spatual to parry the blow, ignoring the hit as if it didn't land."}}
-            });
-            ilib.SaveLibrary();
-
-            ItemLibrary ilib2 = new ItemLibrary();
-            ilib2.LoadLibrary();
             return View();
+        }
+
+        public IActionResult CreateEffect()
+        {
+            return View();
+        }
+
+        public IActionResult SaveEffect()
+        {
+
+            return View("CreateEffect");
         }
 
         public IActionResult Privacy()
