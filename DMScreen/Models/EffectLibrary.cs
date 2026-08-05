96ddd59dd79f1d6e3d31c39eb5f3d940f152e2cf
Pojean <mijn@outlook.dk>
2026-08-05 12:14:28 +0200

    EffectLibrary and Itemlibrary created and FileIO adjusted for working with JSONs

---
+++ DMScreen/Models/EffectLibrary.cs
@@ -0,0 +1,27 @@
+﻿using DMScreen.Services;
+using System.Text.Json;
+
+namespace DMScreen.Models
+{
+    public class EffectLibrary
+    {
+        public List<Effect> effectsLibrary { get; set; }
+
+        public EffectLibrary()
+        {
+         effectsLibrary = new List<Effect>();  
+        }
+
+        public void SaveLibrary()
+        {
+            FileIO.SerialiseEffectLibrary(this);
+        }
+
+        public void LoadLibrary()
+        {
+            string jsonLibrary = FileIO.ReadFile("Effects.json");
+            EffectLibrary local = JsonSerializer.Deserialize<EffectLibrary>(jsonLibrary);
+            effectsLibrary = local.effectsLibrary;
+        }
+    }
+}
--- DMScreen/Models/Item.cs
+++ DMScreen/Models/Item.cs
@@ -3,12 +3,14 @@
     public class Item
     {
         public string Name { get; set; }
+        public string Rarity { get; set; }
+        public int EffectSlots { get; set; }
         public string Description { get; set; }
-        public List<string> Effects { get; set; }
+        public List<Effect> Effects { get; set; }
 
         public Item()
         {
-            Effects = new List<string>();
+            Effects = new List<Effect>();
         }
     }
 }
